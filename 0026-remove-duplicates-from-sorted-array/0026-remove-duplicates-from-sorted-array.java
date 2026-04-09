77428557aa6feaa38683ad461f3fc2975a1132b7
Mayuri Bodade <mayuribodade21@gmail.com>
2026-04-09 22:56:26 +0530

    Time: 1 ms (77.22%), Space: 46.5 MB (85.49%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       int n = nums.length;
+       if(n==0){
+        return 0;
+       }
+       int j=1;
+       for(int i=0 ; i<n-1 ; i++){
+        if(nums[i]!=nums[i+1]){
+            nums[j] = nums[i+1];
+            j++;
+        }
+       }
+       return j;
+    }
+}
