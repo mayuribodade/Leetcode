3f3ff8c7c1351c4c0122b3331b2ddae4d7dddef8
Mayuri Bodade <mayuribodade21@gmail.com>
2026-04-12 12:30:43 +0530

    Time: 1 ms (99.92%), Space: 94.4 MB (75.72%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+       
+        for(int i=0 ; i<prices.length; i++) {
+
+            // if(prices[i] < min){
+            //     min = prices[i];
+            // }
+
+            min = Math.min(min , prices[i]);
+            int profit = prices[i] - min ;
+
+            maxProfit = Math.max(maxProfit , profit);
+
+        }
+        return maxProfit;
+    }
+}
