ed4093f6e0e621d6a5c6bd1c1c9097e0f8dcffb7
Mayuri Bodade <mayuribodade21@gmail.com>
2026-04-20 12:06:44 +0530

    Time: 1 ms (97.82%), Space: 42.9 MB (11.52%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if(x < 2){
+            return x;
+        }
+        int ans = 0;
+        int low = 0;
+        int high = x;
+        while(low <= high){
+            int mid = low + (high - low) /2;
+            long expect = (long) mid * mid; // Use long to prevent overflow
+
+            if(expect == x){
+                return mid;
+            }
+            else if(expect > x){
+                high = mid -1 ;
+            }
+            else{
+                low = mid +1;
+                ans = mid ;// Store mid because it's a potential floor answer
+            }
+        }
+        return ans;
+    }
+}
