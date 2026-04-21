c9653297a49deb59f006a43e6697ee2dc155b33a
Mayuri Bodade <mayuribodade21@gmail.com>
2026-04-21 18:54:37 +0530

    Time: 1 ms (88.7%), Space: 44.2 MB (60.14%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, 0 , new ArrayList<>() , result);
-        return result;
+       List<List<Integer>> result = new ArrayList<>();
+    
+      logic(nums ,0 , new ArrayList<>(), result);
+
+      return result;
+
     }
-    public void backtrack(int [] nums , int start , List<Integer> currentlist , List<List<Integer>> result){
-        //add empty brackets
-        result.add(new ArrayList<>(currentlist));
+    public void logic(int[] nums ,int start , List<Integer> current , List<List<Integer>> result){
+        //make 2d list to store ans
+        result.add(new ArrayList<>(current));
+        
+        for(int i= start ; i<nums.length ; i++){
+            //add current including elem
+            current.add(nums[i]);
 
-        for(int i=start ; i<nums.length ; i++){
-            //add current elemt
-            currentlist.add(nums[i]);
-            //backtrack to add more elemt
-            backtrack(nums , i+1 , currentlist , result);
+            logic(nums, i+1 , current , result);
 
-            //not including that number
-            currentlist.remove(currentlist.size() -1);// remove last elem
+            //not including elem
+            current.remove(current.size()-1);
         }
     }
+    
 }
