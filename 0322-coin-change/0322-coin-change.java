42d4dcb916dfa76a5bdab80ca0e0b41161014068
Mayuri Bodade <mayuribodade21@gmail.com>
2026-04-21 19:12:08 +0530

    Time: 10 ms (99.81%), Space: 45.9 MB (97.79%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount+1;
-        int[] dp = new int [amount+1];
-        Arrays.fill(dp ,max);
-        dp[0] = 0; // 0 coins to make amt 0 , -> use no coin
-        for(int c : coins){
-            for(int j=c ; j<=amount ; j++){
-                dp[j] = Math.min(dp[j] , dp[j-c] +1);
-            }
-        }
-       if(dp[amount] >amount){
+
+        //minimum coins to make amount
+
+        //initial max value = for this max valye will not be grater than amount 
+       int max = amount+1;
+       int [] dp = new int [max];
+
+       //fill array by max value initially
+       Arrays.fill(dp , max);
+
+       dp[0] = 0 ;// 0 ways to make amt 0
+
+       for(int c : coins){
+        for(int j = c ; j<=amount ; j++){
+            dp[j] = Math.min(dp[j] , dp[j-c]+1);
+          }
+       }
+       if(dp[amount]>amount){
         return -1;
        }
        else{
