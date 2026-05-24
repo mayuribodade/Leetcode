f9ca36d9d4841bdf37c2e654647f2134e9ba7d27
Mayuri Bodade <mayuribodade21@gmail.com>
2026-05-24 13:12:25 +0530

    Time: 2 ms (91.7%), Space: 47.8 MB (36.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,10 +1,10 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-         int j = 0;
-        for(int i =0 ;i<nums.length ; i++){
+        int j = 0;
+        for(int i=0 ; i<nums.length ; i++){
             if(nums[i]!=0){
-                //two pointer
-                int temp =nums[i];
+                //swap i and j elem
+                int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
                 j++;
