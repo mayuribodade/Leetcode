7e149671ec4c827a6a7787f652cff0bd5b7386b6
Mayuri Bodade <mayuribodade21@gmail.com>
2026-06-09 10:07:18 +0530

    Time: 2 ms (99.64%), Space: 43.2 MB (39.41%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,31 +1,33 @@
 class Solution {
     public String countAndSay(int n) {
-        String s = "1";
-        for(int i=2 ; i<=n ; i++){
-            s = countAndAdd(s);
-        }
-        return s;
+        // Base case for 1st term sequence
+       String s  = "1";
+
+       for(int i=2 ; i<=n ; i++){
+          s = countAndAdd(s);
+       }
+       return s;
     }
     //helper function
     private String countAndAdd(String s){
         StringBuilder newstr = new StringBuilder();
-
         char c = s.charAt(0);
         int count = 1;
-
         for(int i=1 ; i<s.length() ; i++){
             if(s.charAt(i) == c){
-                count++;
+                count++; //same character
             }
             else{
                 newstr.append(count);
                 newstr.append(c);
-                c = s.charAt(i);
-                count = 1;
+                c = s.charAt(i); //new character
+                count = 1; // reset
             }
         }
-               newstr.append(count);
-                newstr.append(c);
-                return newstr.toString();
+        //add last group also
+         newstr.append(count);
+         newstr.append(c);
+
+         return newstr.toString();
     }
 }
