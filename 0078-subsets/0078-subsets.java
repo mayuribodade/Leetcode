06a6860627413aa2944528ebbf53f521173aa574
Mayuri Bodade <mayuribodade21@gmail.com>
2026-06-16 16:05:04 +0530

    Time: 1 ms (88.82%), Space: 44 MB (73.95%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
        List<List<Integer>> result = new ArrayList<>();
-    
-      logic(nums ,0 , new ArrayList<>(), result);
 
-      return result;
+       logic(nums , 0 , new ArrayList<>() , result);
 
+       return result;
     }
-    public void logic(int[] nums ,int start , List<Integer> current , List<List<Integer>> result){
-        //make 2d list to store ans
+    public void logic (int[] nums , int start , List<Integer> current , List<List<Integer>> result){
+        //add result to 2d list
         result.add(new ArrayList<>(current));
-        
-        for(int i= start ; i<nums.length ; i++){
-            //add current including elem
+
+        for(int i=start ; i<nums.length ; i++){
+            //YES choice
             current.add(nums[i]);
 
-            logic(nums, i+1 , current , result);
+            logic(nums, i+1 , current ,result );
 
-            //not including elem
+            //remove elem NO choice
             current.remove(current.size()-1);
         }
     }
-    
 }
