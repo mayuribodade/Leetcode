85ba40643fc523cb77057694a7fad1b922e9f2a8
Mayuri Bodade <mayuribodade21@gmail.com>
2026-06-23 13:46:15 +0530

    Time: 47 ms (34.47%), Space: 43 MB (94.4%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
@@ -1,25 +1,23 @@
 class Solution {
-    int [][] dp;
-
+    int[][]dp ;
     private int solve(int left , int right){
         if(left>=right){
             return 0;
         }
-        if(dp[left][right]!=0){
+        if(dp[left][right] != 0){
             return dp[left][right];
         }
-
         int ans = Integer.MAX_VALUE;
         for(int i=left ; i<=right ; i++){
-            int cost = i+ Math.max(solve(left , i-1) , solve(i+1,right));
+            int cost = i+ Math.max(solve(left,i-1) ,solve(i+1,right));
+
             ans = Math.min(ans , cost);
         }
         dp[left][right] = ans;
         return ans;
     }
     public int getMoneyAmount(int n) {
-        //creating dp to store values
-        dp = new int [n+1][n+1]; 
-        return solve(1,n);
+      dp = new int[n+1][n+1];
+      return solve(1,n);
     }
 }
