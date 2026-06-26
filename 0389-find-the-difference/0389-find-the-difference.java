b95d41e937917058ecb3761f64035ed0ee052b92
Mayuri Bodade <mayuribodade21@gmail.com>
2026-06-26 13:57:42 +0530

    Time: 1 ms (99.91%), Space: 43.1 MB (71.11%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -2,10 +2,12 @@ class Solution {
     public char findTheDifference(String s, String t) {
       char ans = 0;
        for(char ch : s.toCharArray()) {
-        ans = (char)(ans^ch);
+       // ans = (char)(ans^ch);\
+       ans^= ch;
        }
        for(char ch : t.toCharArray()) {
-        ans = (char)(ans^ch);
+       // ans = (char)(ans^ch);
+       ans^= ch;
        }
        //ans contains extra eleme only
        return ans;
