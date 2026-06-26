a64715a29dc4518e1f797d3df58355f362ed80fd
Mayuri Bodade <mayuribodade21@gmail.com>
2026-06-26 13:49:11 +0530

    Time: 16 ms (7.35%), Space: 43.3 MB (19.52%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,15 +1,20 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character , Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) + 1);
-        }
-        for(int i=0; i<t.length(); i++){
-            if(!map.containsKey(t.charAt(i)) || map.get(t.charAt(i))<=0) return t.charAt(i);
-            else{
-                map.put(t.charAt(i) , map.getOrDefault(t.charAt(i) , 0) -1);
-            }
-        }
-        return 'a';
+       Stack<Character> stack = new Stack<>();
+       for(int i=0 ; i<s.length() ;i++){
+            stack.push(s.charAt(i));
+       }
+       for(int i=0 ; i<t.length() ; i++){
+        char ch = t.charAt(i);
+          if(stack.contains(ch)){
+            //// Removes the specific character found in stack
+            stack.remove(stack.lastIndexOf(ch));
+          }
+          else{
+            //stack doesn't contains that element means it is extra one
+            return ch;
+          }
+       }
+       return ' ';
     }
 }
