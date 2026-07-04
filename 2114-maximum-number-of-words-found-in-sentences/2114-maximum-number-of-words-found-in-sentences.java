f2f29ee93181038f10f2f2d912bc535ec46f2736
Mayuri Bodade <mayuribodade21@gmail.com>
2026-07-04 13:38:52 +0530

    Time: 3 ms (89.94%), Space: 46.1 MB (36.6%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -3,9 +3,9 @@ class Solution {
         int max = 0;
         for(int i=0 ; i<sentences.length ; i++){
             String s = sentences[i];
-            String [] sWords = s.split(" ");
+            String [] sword = s.split(" ");
 
-            max = Math.max(max , sWords.length);
+            max = Math.max(max , sword.length);
         }
         return max;
     }
