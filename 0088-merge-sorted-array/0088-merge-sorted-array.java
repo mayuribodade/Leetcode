ac3d86dd8d8d4189e81bf063d77d7eb750584656
Mayuri Bodade <mayuribodade21@gmail.com>
2026-07-12 13:22:23 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       /* for(int i=0 ; i<n ; i++){
-            nums1[m] = nums2[i];
-            m++;
-        }
-        Arrays.sort(nums1);*/
-        //THREE POINTER METHOD
+      int i = m-1; 
+      int j = n-1;
+      int k = (m+n)-1;
 
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-
-        while(j>=0){//ham nums2 ko 1 me dalre aUR SATH ME SORT BHI KRRE
-               if(i>=0 && nums1[i] > nums2[j]){
-                //back se elemnt store karege
-                nums1[k] = nums1[i];
-                k--;
-                i--;
-               }
-               else{
-                nums1[k] = nums2[j];
-                k--;
-                j--;
-               }
-        }
+        //sort from backward
+      while(j>=0){
+          if(i>=0 && nums1[i] > nums2[j]){
+            nums1[k] = nums1[i];
+            i--;
+            k--;
+          }
+          else{
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+          }
+      }
+    
     }
 }
