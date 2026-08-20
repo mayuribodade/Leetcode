a13d407c4d4006852e0641f0cc8fbc73fa7c7611
Mayuri Bodade <mayuribodade21@gmail.com>
2026-08-20 11:14:21 +0530

    Time: 1 ms (99.95%), Space: 77.3 MB (49.28%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int ms = Integer.MIN_VALUE;
-        int cs  =0 ;
-        for(int i=0 ; i<nums.length ; i++){
-            cs += nums[i];
+       int cs = 0;
+       int ms = Integer.MIN_VALUE;
 
-            ms = Math.max(ms,cs);
+       for(int i=0 ; i<nums.length ; i++){
+         cs += nums[i];
 
-            if(cs<0){
-                cs = 0;
-            }
-        }
-        return ms;
+          ms = Math.max(cs , ms);
+
+         if(cs < 0) cs = 0;
+       }
+       return ms;
     }
 }
