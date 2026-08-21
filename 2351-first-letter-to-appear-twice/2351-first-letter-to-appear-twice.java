579260d1106be2596db9957de1b3214185fbd586
Mayuri Bodade <mayuribodade21@gmail.com>
2026-08-21 10:42:46 +0530

    Time: 1 ms (15.95%), Space: 42.6 MB (86.4%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,19 +1,13 @@
 class Solution {
     public char repeatedCharacter(String s) {
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character , Integer> map = new HashMap<>();
+       for(char ch : s.toCharArray()){
+        map.put(ch , map.getOrDefault(ch,0)+1);
 
-        for(int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-
-            if(map.get(ch) == 2) {
-                return ch;
-            }
-        }
-
-        return 'z';
+        if(map.get(ch) == 2) return ch;
+       }
+    
+     return 'z';
     }
 }
