5df0eb40fcb391847d692469c174d85fd1191447
Mayuri Bodade <mayuribodade21@gmail.com>
2026-08-23 21:13:12 +0530

    Time: 5 ms (66.28%), Space: 47.2 MB (52.94%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        //base case 
+        // Base case 1: Fill the first row up to column m
+        for(int j=0;j<=m ;j++){
+           dp[0][j] = j ;
+        }
+        // Base case 2: Fill the first column up to row n
+        for(int i=0 ;i<=n ; i++){
+            dp[i][0] = i;
+        }
+
+        for(int i=1 ; i<=n  ; i++){
+            for(int j=1 ; j<=m ; j++){
+                //agar char match ho to sirf uska topleft(diagonal) dekho
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                     dp[i][j] = dp[i-1][j-1];
+                }
+
+                else{
+                    //uske topleft , top aur left elemet ka min lo
+                    int topleft = dp[i-1][j-1];
+                    int top = dp[i-1][j];
+                    int left = dp[i][j-1];
+
+                    dp[i][j] = Math.min(topleft , Math.min(top , left))+1;
+                }
+               
+            }
+        }
+        return dp[n][m];
+    }
+}
