e716de21bbe164c9159cd312c052a6db32670eba
Mayuri Bodade <mayuribodade21@gmail.com>
2026-08-27 16:22:33 +0530

    Time: 6 ms (43.56%), Space: 44.5 MB (59.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
 
-       if(s.length() != t.length()){
-            return false;
-        }
-        /*
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        return Arrays.equals(arr1,arr2);*/
-
-        int[] count = new int[26];
+        int[] bucket = new int[26];
 
         for(int i=0 ; i<s.length() ; i++){
-           count[ s.charAt(i) - 'a']++;
-           count[ t.charAt(i) - 'a']--;
+            bucket[s.charAt(i) - 'a']++;
+            bucket[t.charAt(i) - 'a']--;
         }
-        for(int i=0 ; i<count.length ; i++){
-            if(count[i]!=0){
-                return false;
-            }
+        for(int i=0 ; i<26 ;i++){
+            if(bucket[i] !=0) return false;
         }
-       return true;
+        return true;
     }
 }
