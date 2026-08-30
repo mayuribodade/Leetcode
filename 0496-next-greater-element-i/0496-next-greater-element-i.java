31f40748834adb103119f1791f61564a09005c1b
Mayuri Bodade <mayuribodade21@gmail.com>
2026-08-31 00:01:58 +0530

    Time: 4 ms (74.07%), Space: 45.2 MB (49.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,23 +2,25 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer , Integer> map = new HashMap<>();
 
-       //since nums2 contains all element of nums1 also
-        for(int i=0 ; i<nums2.length ; i++){
-          int greater = -1;
-          for(int j=i+1 ; j<nums2.length ; j++){
-            if(nums2[j]>nums2[i]){
-                greater = nums2[j];
-                break;
+        Stack<Integer> s = new Stack<>();
+
+        for(int i= nums2.length-1 ; i>=0 ; i--){
+            while(!s.isEmpty() && s.peek() <= nums2[i]){
+                s.pop();
             }
-          }
-          map.put(nums2[i] , greater);
+            if(s.isEmpty()){
+                map.put(nums2[i] , -1);
+            }
+            else{
+                map.put(nums2[i] , s.peek());
+            }
+            s.push(nums2[i]);
         }
 
-        //now store in array
-        int[] a = new int[nums1.length];
-        for(int i=0 ; i<a.length ; i++){
-            a[i] = map.get(nums1[i]);// it will give present elements greater elem from map
+        int [] ans = new int[nums1.length];
+        for(int i=0 ;i<nums1.length ; i++){
+            ans[i] = map.get(nums1[i]);
         }
-        return a;
+        return ans;
     }
 }
