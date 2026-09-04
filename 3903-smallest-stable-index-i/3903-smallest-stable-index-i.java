ea89685f884f8d34451dfb6610766d89b90e7f68
Mayuri Bodade <mayuribodade21@gmail.com>
2026-09-04 11:44:40 +0530

    Time: 1 ms (99.79%), Space: 46.2 MB (70.85%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+        int n = nums.length;
+
+          int []maxi = new int[nums.length];
+          int []mini = new int[nums.length];
+
+          int max = Integer.MIN_VALUE;
+          int min = Integer.MAX_VALUE;
+       
+         
+        //build suffix maxi array
+        for(int i=0 ; i<n ; i++){
+            max = Math.max(max , nums[i]);
+            maxi[i] = max;
+        }
+
+     //The suffix minimum array must be built backwards from n - 1 down to 0 because each entry at index i depends on the minimum value of everything to its right (from index i to n - 1).
+        //build suffix mini array
+        for(int i = n-1 ; i>=0 ; i--){
+            min = Math.min(min , nums[i]);
+            mini[i] = min;
+        }
+       for(int i = 0 ;i<n ; i++){
+        if(maxi[i] - mini[i] <= k){
+            return i;
+         }
+       }
+       return -1;
+    }
+}
