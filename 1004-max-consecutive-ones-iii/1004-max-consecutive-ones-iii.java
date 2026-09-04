858a1ee6ef92a614864c567af2bcfed84a98211a
Mayuri Bodade <mayuribodade21@gmail.com>
2026-09-04 23:47:55 +0530

    Time: 4 ms (44.34%), Space: 52.1 MB (54.63%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -7,11 +7,13 @@ class Solution {
       for(int right= 0 ; right<nums.length ; right++){
         if(nums[right] == 0) zerocount++;
 
-        while(zerocount > k){
+        if(zerocount > k){
             if(nums[left] == 0) zerocount--;
             left++;
         }
+        if(zerocount <= k){
         maxlength = Math.max(maxlength , right-left+1);
+        }
       }
       return maxlength;
     }
