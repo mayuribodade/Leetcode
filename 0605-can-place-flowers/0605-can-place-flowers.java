c676a0398dafbca101f14e0d5ce741bb80b60ba4
Mayuri Bodade <mayuribodade21@gmail.com>
2026-09-24 11:39:07 +0530

    Time: 1 ms (98.67%), Space: 47.5 MB (88.25%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] nums, int n) {
+        int count = 0;
+        int k = nums.length;
+
+        for(int i=0 ; i<k ; i++){
+           if(nums[i] == 0){
+            //check  its left neighbour
+            boolean left = false;
+            if(i==0 || nums[i-1] == 0) left =true;
+
+            boolean right = false;
+            if(i==k-1 || nums[i+1] == 0) right = true;
+
+            if(left && right){
+                nums[i] = 1;
+                count++;
+            }
+           }
+        }
+        if(count >= n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
